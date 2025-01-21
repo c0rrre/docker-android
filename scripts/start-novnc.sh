f6b84db1c67f2b00b4215d0e37eb474932b30590
c0rrre <joergbuschmann26@gmail.com>
2025-01-21 11:27:33 +0100

    install x11vnc and remove unused images

--- scripts/start-novnc.sh
+++ scripts/start-novnc.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Start websockify to bridge WebSocket to VNC server (localhost:5900)
-echo "Starting websockify..."
-websockify --web /usr/share/novnc/ 6080 localhost:5900 &
-echo "noVNC is now available on port 6080."
-
 # Wait for the Android emulator to be ready
 echo "Waiting for the Android emulator to be ready..."
 sleep 5
 
 # Start the VNC server (if not already started)
 echo "Starting VNC server..."
-x11vnc -forever -usepw -display :0 &
+x11vnc -display :0 -forever -usepw -listen localhost -noipv6 &
+
+# Start websockify to bridge WebSocket to VNC server (localhost:5900)
+echo "Starting websockify..."
+websockify --web /usr/share/novnc/ 6080 localhost:5900 &
 
 # Start noVNC (the client that connects to websockify)
 echo "Starting noVNC..."
